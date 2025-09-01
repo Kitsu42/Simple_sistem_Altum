56ee7136398e2218f396025e3ba105c44ad01e26
Kitsu <kitsucode@gmail.com>
2025-08-31 21:25:00 -0300

    goodbye project

--- backend/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user import User, UserRole
-from .organization import Empresa, Filial
-from .requisicao import Requisicao, StatusRC
--- frontend/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-green-100">
-      <h1 className="text-2xl font-bold">Bem-vindo ao Dashboard!</h1>
-    </div>
-  );
-};
-
-export default Dashboard;
